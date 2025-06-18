51d5412961767c864f270701e54c806876a1b006
Bernard <bernard.realino98@gmail.com>
2025-06-18 10:15:31 +0700

    V1.1: - added add duration each file

--- public/server.js
+++ public/server.js
@@ -1,7 +1,6 @@
-
-const WebSocket = require('ws');
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const WebSocket = require("ws");
+const path = require("path");
 
 const app = express();
 const port = 3000;
@@ -9,18 +8,23 @@ const port = 3000;
 app.use(express.static(path.join(__dirname)));
 
 const server = app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
 
 const wss = new WebSocket.Server({ server });
-let connections = [];
 
-wss.on('connection', function connection(ws) {
-  connections.push(ws);
-  ws.on('message', function incoming(message) {
-    connections.forEach(conn => conn.send(message));
+let clients = [];
+
+wss.on("connection", ws => {
+  clients.push(ws);
+  ws.on("close", () => {
+    clients = clients.filter(client => client !== ws);
   });
-  ws.on('close', () => {
-    connections = connections.filter(conn => conn !== ws);
+  ws.on("message", message => {
+    clients.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
   });
 });
